613df9bc2f567e5e1d2b709205f0a7d116a2a8cb
Cangjier <moodlee@qq.com>
2024-12-19 03:50:52 +0000

    optimize

--- cli/workspace-get-documents/index.ts
+++ cli/workspace-get-documents/index.ts
@@ -266,7 +266,6 @@ let main = async () => {
         }
     })();
     await taskUtils.whenAll([tasks1, tasks2]);
-    console.log(`remote documents : ${remoteDocuments}`);
     let resultDocuments = [] as IDocumentRecord[];
     output.Documents = resultDocuments;
     for (let document of localDocuments) {
@@ -287,7 +286,6 @@ let main = async () => {
         }
         resultDocuments.push(document);
     }
-    output.Documents = localDocuments;
     remoteDocuments.forEach(item => {
         resultDocuments.push(item);
     });
